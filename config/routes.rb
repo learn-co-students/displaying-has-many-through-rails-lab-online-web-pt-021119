90c068efc00443b6da94ac2368462c4f456f26d3
Berlinta <nashfft@gmail.com>
2019-07-28 15:01:49 -0700

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'patients/index'
+
+  get 'doctors/index'
+  get 'doctors/show'
+  get 'appointments/show'
+  get 'patients/index'
+  get 'patients/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
