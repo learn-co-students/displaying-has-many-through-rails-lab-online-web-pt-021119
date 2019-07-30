c158d5f28ee1564747b7029668b46bf3da6d2ab3
github username <github email address>
2019-07-30 17:54:55 -0600

    Done.

---
+++ db/migrate/20190730231843_change_appointment_datetime_to_be_datetime_in_appointments.rb
@@ -0,0 +1,6 @@
+class ChangeAppointmentDatetimeToBeDatetimeInAppointments < ActiveRecord::Migration[5.0]
+  def change
+    change_column :appointments, :appointment_datetime, :datetime
+  end
+
+end
